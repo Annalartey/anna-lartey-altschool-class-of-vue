a5cd4a845325eb610c2929d8d5120d670bfb2699
Annalartey <74235952+Annalartey@users.noreply.github.com>
2023-02-08 16:33:04 +0000

    added css

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,11 +1,17 @@
 <template>
-  <h1>Color Picker Game</h1>
+ <div class="game-bg">
+    <h1>Color Picker Game</h1>
 
-  <div>{{ message }}</div>
+    <div>{{ message }}</div>
 
-  <button v-for="color in colors" :key="color" @click="matchColor(color)">
-    {{ color }}
-  </button>
+    <button 
+      v-for="color in colors" 
+      :key="color" 
+      @click="matchColor(color)">
+      {{ color }}
+    </button>
+ </div>
+
 </template>
 
 <script>
@@ -18,4 +24,28 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.game-bg{
+    background: #b7b713;
+    padding: 1rem;
+    font-size: 1.5rem;
+    color: black;
+    width: 600px;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 40px;
+}
+h1{
+  border-bottom: 1px solid white;
+  margin: 10px 0;
+}
+button{
+  padding: 8px 12px;
+  border-radius: 8px;
+
+}
+
+</style>
